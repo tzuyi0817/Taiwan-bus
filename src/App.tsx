07d72085f2ea6eef303f18cdcd9b7ebf67aca5eb
tzuyi <a0988426059@gmail.com>
2023-01-17 15:42:27 +0800

    feat BusFooter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Routes from '@/router';
 import BusHeader from '@/components/common/BusHeader';
+import BusFooter from '@/components/common/BusFooter';
 
 function App() {
   return (
     <div>
       <BusHeader />
       <Routes />
+      <BusFooter />
     </div>
   )
 }
---
+++ src/components/common/BusFooter.tsx
@@ -0,0 +1,9 @@
+function BusFooter() {
+  return (
+    <footer className="w-full h-14 fixed left-0 bottom-0 text-white text-center leading-[56px] bg-gray-700">
+      Copyright © 2023 台灣公車 e 點通 All rights reserved.
+    </footer>
+  )
+}
+
+export default BusFooter;
