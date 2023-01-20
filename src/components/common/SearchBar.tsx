77b8d102fb16a504ebd5b58d47b27b45a91a4af5
tzuyi <a0988426059@gmail.com>
2023-01-20 15:18:31 +0800

    fix SearchBar icon

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,32 +1,28 @@
-import {
-  useCallback,
-  type Dispatch,
-  type SetStateAction,
-} from 'react';
+import { type Dispatch, type SetStateAction } from 'react';
 import { createImageSrc } from '@/utils/images';
-import { debounce } from '@/utils/common';
 
 interface Props {
   placeholder: string;
+  keyword: string;
   setKeyword: Dispatch<SetStateAction<string>>;
 }
 
-function SearchBar({ placeholder, setKeyword }: Props) {
-  const handlerSearch = useCallback(debounce(setKeyword), []);
+const icon = 'absolute top-1/2 right-3 -translate-y-1/2';
 
+function SearchBar({ placeholder, keyword, setKeyword }: Props) {
   return (
     <div className="relative">
       <input
         type="text"
         className="searchBar"
-        onChange={(event) => handlerSearch(event.target.value)}
+        value={keyword}
+        onChange={(event) => setKeyword(event.target.value)}
         placeholder={placeholder}
       />
-      <img
-        src={createImageSrc('icon/search.png')}
-        alt=""
-        className="absolute top-1/2 right-6 -translate-y-1/2"
-      />
+      {keyword 
+        ? <span className={`${icon} cursor-pointer hover:text-primary text-base transition-colors`} onClick={() => setKeyword('')}>✖</span>
+        : <img src={createImageSrc(`icons/${keyword ? 'close' : 'search'}.png`)} alt="" className={icon} />
+      }
     </div>
   )
 }
--- src/components/searchBus/SearchBusBlock.tsx
+++ src/components/searchBus/SearchBusBlock.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import SearchBar from "@/components/common/SearchBar";
+import { debounce } from '@/utils/common';
 
 function SearchBusBlock() {
   const [keyword, setKeyword] = useState('');
+  // const handlerSearch = useCallback(debounce(setKeyword), []);
 
   return (
     <div className="bg-white p-5">
-      <SearchBar placeholder="輸入公車路線 / 起迄方向名或關鍵字" setKeyword={setKeyword} />
+      <SearchBar placeholder="輸入公車路線 / 起迄方向名或關鍵字" keyword={keyword} setKeyword={setKeyword} />
     </div>
   )
 }
